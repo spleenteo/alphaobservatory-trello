f9f89b2590c067af28cde36e92844789ddca995e
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2018-03-06 23:22:15 +0100

    Script completed with fake data

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source "https://rubygems.org"
 
 gem 'dotenv'
+gem 'faker'
 gem 'pp'
 gem 'ruby-trello'
--- alphatrello.rb
+++ alphatrello.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 require 'date'
-require 'time'
 require 'dotenv'
+require 'faker'
 require "pp"
 require "pry"
+require 'time'
 require 'trello'
 
 
@@ -13,45 +14,62 @@ def header(content)
   puts "-----------------------------"
 end
 
-
 Dotenv.load
-Trello.configure do |config|
-  header(ENV["TRELLO_DEV_TOKEN"])
-  config.developer_public_key = ENV["TRELLO_TOKEN"]
-  config.member_token = ENV["TRELLO_DEV_TOKEN"] # The token from step 2.
-end
 
-# pp Trello::Board.find("txJrqitV").labels
-# pp Trello::Board.find("txJrqitV").labels
+client = Trello::Client.new(
+  developer_public_key: ENV["TRELLO_TOKEN"],
+  member_token: ENV["TRELLO_DEV_TOKEN"]
+)
 
 @l_generic = ENV['TRELLO_LABEL_GENERIC']
 @l_meeting = ENV['TRELLO_LABEL_MEETING']
-@l_survey = ENV['TRELLO_LABEL_SURVEY']
+@l_survey  = ENV['TRELLO_LABEL_SURVEY']
 
-coll = [@l_generic].join(",")
+coll = [@l_survey, @l_meeting].join(",")
+
+date_meeting  = nil
+date_training = nil
+date_meeting  = Time.parse("2018-03-15 16:15:59")
+date_training = Time.parse("2018-03-10 09:45:00")
+date_test = nil
+
+meeting_dates = []
+meeting_dates.push(date_meeting) if !date_meeting.nil?
+meeting_dates.push(date_training) if !date_training.nil?
+meeting_dates.push(date_test) if !date_test.nil?
+
+first_date = meeting_dates.any? ? meeting_dates.sort[0] : nil
 
 desc =[
   "https://survey.alphaobservatory.org/it/admin/interviews/0e2e204a-9007-479b-a066-5ff0582f7c1f",
-  "Country: Pakistan",
-  "Language: En",
-  "Skype: pippo@pluto",
+  "Country: #{Faker::Address.country}",
+  "Language: #{Faker::Address.country_code}",
+  "Skype: #{Faker::Internet.free_email}",
+  "Meeting date: #{date_meeting}",
+  "Training date: #{date_training}"
 ].join("\n")
 
-d = Time.parse("2018-03-06 19:15:59")
+username = Faker::Name.name
+interviewer = Faker::Ancient.god
 
 pp desc
-pp d
+puts "----------"
+pp meeting_dates[0]
 pp coll
+pp meeting_dates
+pp first_date
 
-options = {
+card = client.create(:card, {
   board_id: ENV["TRELLO_BOARD"],
   list_id: ENV["TRELLO_LIST"],
-  name: 'Generino, 3 Mar, (Loucamerini) ::Activist',
+  name: "#{username}, 3 Mar, (#{interviewer}) ::Activist",
   card_labels: coll,
-  due: d.to_date,
+  due: first_date,
   desc: desc,
   pos: "top"
-}
-c = Trello::Card.create(options)
+})
 
-pp c
+header 'Card created!'
+puts card.url
+
+# pp card
